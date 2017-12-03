3240635b35956a5193491d2ff0a3d3898f6bc865
Yuktheshwar <yuktheshwarbhat@gmail.com>
2017-12-03 21:31:31 +0530

    Update scroll_name _on_lcd.c

--- scroll_name _on_lcd.c
+++ scroll_name _on_lcd.c
@@ -1,23 +1,28 @@
 #include <reg51.h>
 #include"delay_ms.h"
 #include"lcd8bit.h"
-code char name[5]="amal";
-void main()
+void lcd_scrol(unsigned char *s)
 {
-	unsigned char i,j;
+    unsigned char name[20]="";
+	unsigned char i=0,j=0;
+	while(*s)
+	 {
+	 j++;
+	 name[i]=*s++;
+	 i++;
+	 }
 	lcd_init();
 	lcd_cmd(0x0c);//lcd cursor off
 	while(1)
 	{
-			j=4;//length of the string to scroll
 	for(i=0;i<16;i++)
 	{
 		lcd_cmd(0x01);//clear lcd screen
 		if((i+j+1)>16)  //To check whether name exceeds first line
 		{
-		   lcd_cmd(0x80);
+		     lcd_cmd(0x80);
 			 lcd_string(name+j);
-			j--;
+			 j--;
 		}
 		lcd_cmd(0x80+i);		
 		lcd_string(name);
@@ -25,3 +30,4 @@ void main()
 	}
   }
 }
+
